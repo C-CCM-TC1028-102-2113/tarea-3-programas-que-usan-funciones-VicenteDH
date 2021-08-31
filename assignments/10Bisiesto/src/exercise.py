f6afdaad987cf77cb12bc94aca1dc850bcab8f84
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-08-30 21:31:39 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,19 @@
+def bis(a):
+    if (a%4==0 and a%100==0) or (a%400==0):
+        b=1
+    else:
+        b=0
+    return b
 
 def main():
-    #escribe tu código abajo de esta línea
+    año=int(input())
+    c=bis(año)
+    if c==1:
+      print("True")
+    elif c==0:
+        print("False")
     pass
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
