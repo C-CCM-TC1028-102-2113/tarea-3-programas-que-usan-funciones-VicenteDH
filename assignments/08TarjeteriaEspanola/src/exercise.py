4dab6deb627d644828483aad7563c0315a8c5bbb
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-08-30 21:07:07 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,11 +1,16 @@
-papel=int(input("Dame la cantidad de pliegos de papel albanene: "))
-plumones=int(input("Dame la cantidad de plumones: "))
-def main(papel,plumones):
-    total1=papel*12
-    total2=plumones*35
+def main():
+    papel=int(input("Dame la cantidad de pliegos de papel albanene: "))
+    plumones=int(input("Dame la cantidad de plumones: "))
+    c=tarjeta(papel, plumones)
+    print(c)
+    pass
+def tarjeta(pa,pl):
+    total1=pa*12
+    total2=pl*35
     if total1>total2:
         return("El número máximo de tarjetas que se pueden hacer es: "+str(total1))
     elif total2>total1:
         return("El número máximo de tarjetas que se pueden hacer es: "+str(total2))
-resultado=main(papel,plumones)    
-print(resultado)
+
+if __name__ == '__main__':
+    main()
