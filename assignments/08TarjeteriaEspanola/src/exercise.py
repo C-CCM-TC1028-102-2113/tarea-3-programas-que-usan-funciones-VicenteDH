51ea1ed39261545ddb8c1584dcb297449317f1d2
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-08-30 21:08:57 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -4,12 +4,13 @@ def main():
     c=tarjeta(papel, plumones)
     print(c)
     pass
+
 def tarjeta(pa,pl):
     total1=pa*12
     total2=pl*35
-    if total1>total2:
+    if total1<total2:
         return("El número máximo de tarjetas que se pueden hacer es: "+str(total1))
-    elif total2>total1:
+    elif total2<total1:
         return("El número máximo de tarjetas que se pueden hacer es: "+str(total2))
 
 if __name__ == '__main__':
